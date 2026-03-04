8ae49d3b1d9b78b3517ec30853426dd5b5634595
zaskiameizya <zaskiameisyaputri@gmail.com>
2026-03-04 16:11:28 +0700

    perubahan html css

--- halaman1.php
+++ halaman1.php
@@ -4,9 +4,64 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <style>
+        body {
+            margin: 0;
+            font-family: Arial, sans-serif;
+            background: linear-gradient(to right, #1e3c72, #2a5298);
+            display: flex;
+            justify-content: center;
+            align-items:center;
+            height: 100hv;
+        }
+
+        .card {
+            background: #e6f0f8;
+            width: 400px;
+            padding: 30px;
+            text-align: center;
+            border-radius: 15px;
+            box-shadow: 0 10px 25px rgba(0,0,0,0.3);
+        }
+
+        .icon {
+            font-size: 60px;
+            margin-bottom: 10px;
+        }
+
+        h2 {
+            color: #1e3c72;
+            margin-bottom: 10px;
+        }
+
+        p {
+            color: #333;
+            margin-bottom: 20px;
+        }
+
+        button {
+            padding: 10px 20px;
+            background-color: #1e3c72;
+            color: white;
+            border: none;
+            border-radius: 8px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #16325c;
+        }
+        </style>
 </head>
 <body>
-    <h1>Halaman1</h1>
-    <h2>smk n 1 kandeman</h2>
+    <div class="card">
+        <div class="icon"> </div>
+        <h2>Autentifikasi gagal!</h2>
+        <p>NISN dan Kode salah!</p>
+        <button onclick="halaman2.php>
+        COBA LAGI
+        </button>
+</div>
+
 </body>
 </html>
