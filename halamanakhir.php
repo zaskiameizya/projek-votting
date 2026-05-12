d28e2d395672f6b8559446f4186c36782d01e286
marthawahyuni248219-cpu <marthawahyuni248219@student.smkn1kandeman.sch.id>
2026-05-12 09:48:09 +0700

    dsfdf

--- halamanakhir.php
+++ halamanakhir.php
@@ -1,96 +1,100 @@
 <!DOCTYPE html>
 <html lang="id">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Akhir</title>
+    <link rel="stylesheet" href="https://cdn.hugeicons.com/font/hgi-stroke-rounded.css" />
+      
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            font-family:  'Inika', serif;
+        }
 
-<title>E-Voting OSIS</title>
+        body {
+            display: flex;
+            height: 100vh; 
+            background-image: url('BG.jpeg');
+            background-size: cover;
+            color: white;
+            overflow: hidden;
+        }
 
-<style>
+      
+        .sidebar {
+            width: 200px; 
+            background-color: #4c6f7a;
+            padding: 40px 20px;
+            display: flex;
+            flex-direction: column;
+            border-right: 1px solid #2c3e50;
+            box-shadow: 4px 0 15px rgba(0,0,0,0.3);
+            z-index: 10;
+        }
 
-*{
-    margin:0;
-    padding:0;
-    box-sizing:border-box;
-    font-family: 'Inika', serif;
-}
+        .sidebar h2 {
+            font-size: 22px;
+            text-align: center;
+            margin-bottom: 50px;
+            line-height: 1.4;
+            font-family: 'Inika', serif;
+        }
 
-body{
-    background-image: url('BG.jpeg');
-}
+        .nav-links {
+            flex-grow: 1;
+        }
 
-.sidebar{
-    width:150px;
-    height:100vh;
-    background:rgba(255,255,255,0.1);
-    position:fixed;
-    color:white;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-between;
-    z-index:20;
-}
+        .nav-links div {
+            padding: 15px 15px;
+            cursor: pointer;
+            font-size: 15px;
+            margin-bottom: 10px;
+            transition: 0.3s;
+            background: rgba(255,255,255,0.15);
+            border-radius: 12px;
+            font-weight: bold;
+        }
 
-.logo{
-    text-align:center;
-    margin-top:30px;
-    font-size:20px;
-    font-weight:bold;
-    line-height:40px;
-}
+        .nav-links .active {
+            font-weight: bold;
+            color: #f2f4f5;
+            border-radius: 12px;
+            background: transparent;
+        }
 
-.menu{
-    margin-top:40px;
-}
+        .user-info {
+            font-size: 14px;
+            text-align: center;
+            padding-top: 20px;
+            border-top: 1px solid rgba(255,255,255,0.2);
+        }
 
-.menu a{
-    display:block;
-    color:white;
-    text-decoration:none;
-    text-align:center;
-    padding:10px;
-    font-size:18px;
-    font-weight:bold;
-    border-radius:8px;
-}
+        .logout-btn {
+            margin-top: 10px;
+            background-color: #2c3e50;
+            padding: 10px 20px;
+            color: white;
+            cursor: pointer;
+            border-radius: 20px;
+            display: inline-block;
+            outline: 2px solid black;
+        }
 
-.menu a:hover{
-    background:#29536b;
-}
-
-/* BOTTOM */
-.bottom{
-    text-align:center;
-    margin-bottom:30px;
-}
-
-.bottom h2{
-    font-size:18px;
-    margin-bottom:15px;
-}
-
-.logout{
-    display:inline-block;
-    background:#173547;
-    color:white;
-    padding:10px 20px;
-    border-radius:25px;
-    text-decoration:none;
-    font-weight:bold;
-}
-
-.content{
-    margin-left:150px;
-    width:calc(100% - 150px);
-    height:100vh;
-    display:flex;
-    justify-content:center;
-    align-items:center;
-    position:relative;
+        .content{
+            margin-left:150px;
+            width:calc(100% - 150px);
+            height:100vh;
+            display:flex;
+            justify-content:center;
+            align-items:center;
+            position:relative;
     
-}
+        } 
 
-.nav-item {
+        .nav-item {
             padding: 15px 25px;
             cursor: pointer;
             display: block;
@@ -100,106 +104,87 @@ body{
         }
 
         .nav-item2{
-         padding: 15px 25px;
-         cursor:pointer;
-         color: white;
-         text-decoration: none;
+            padding: 15px 25px;
+            cursor:pointer;
+            color: white;
+            text-decoration: none;
         }
 
 
-.card{
-    width:550px;
-    height:240px;
-    background:#698D9F;
-    border-radius:10px;
-    text-align:center;
-    padding-top:50px;
-    position:relative;
-    z-index:2;
-    border: 3px solid #386982;
-}
+        .card{
+            width: 800px;
+            height: 400px;
+            background:#698D9F;
+            border-radius:10px;
+            text-align:center;
+            padding-top:50px;
+            position:relative;
+            z-index:2;
+            border: 3px solid #386982;
+        }
 
-.card h1{
-    color:white;
-    font-size:24px;
-    line-height:42px;
-    font-weight:bold;
-}
+        .card h1{
+            color:white;
+            font-size: 32px;
+            line-height:42px;
+            font-weight:bold;
+            margin-top: 50px;
+        }
 
+        .btn{
+            margin-top:15px;
+            background:#5c6159;
+            color:white;
+            border-radius:18px;
+            padding:12px 28px;
+            font-size:18px;
+            font-weight:bold;
+            cursor:pointer;
+            margin-top: 50px;
+        }
 
-.btn{
-    margin-top:15px;
-    background:#5c6159;
-    color:white;
-    border-radius:18px;
-    padding:12px 28px;
-    font-size:18px;
-    font-weight:bold;
-    cursor:pointer;
-}
+        .students{
+            position:absolute;
+            left:90px;
+            bottom: -50px;
+            width:400px;
+            z-index:3;
+            margin-left: 300px;
+        }
 
-
-
-.students{
-    position:absolute;
-    left:90px;
-    bottom:0;
-    width:340px;
-    z-index:3;
-}
-
-</style>
+    </style>
 </head>
-
 <body>
 
+    <nav class="sidebar">
+        <h2>Sistem<br>E-Voting<br>Osis</h2>
+        <div class="nav-links">
+            <div onclick="window.location.href='halaman4.php'">Voting</div>
+            <div class="active" onclick="window.location.href='hasilvotting.php'">Hasil Voting</div>
+        </div>
+        <div class="user-info">
+            <p>Hi Alletta</p>
+            <div class="logout-btn" onclick="window.location.href='index.php'">Log Out →</div>
+        </div>
+    </nav>
 
-<div class="sidebar">
+     <div class="content">
 
-    <div>
+        <div class="card">
 
-        <div class="logo">
-            Sistem<br>
-            E-Voting<br>
-            Osis
+            <h1>
+                Pilihan anda sudah kami simpan,<br>
+                Terima kasih sudah berpartisipasi<br>
+                dalam voting ini.
+            </h1>
+
+            <button class="btn" onclick="window.location.href='index.php'">KEMBALI KE HALAMAN LOGIN <i class="hgi hgi-stroke hgi-rounded hgi-link-backward"></i></button>
+            
         </div>
 
-        <div class="nav-menu">
-            <a href="#" class="nav-item">Voting</a>
-            <a href="#" class="nav-item2">Hasil Voting</a>
-        </div>
-
     </div>
-
-    <div class="bottom">
-        <h2>Hi Alletta</h2>
-
-        <a href="#" class="logout">
-            Log Out ↪
-        </a>
-    </div>
-
-</div>
-
-
-<div class="content">
-
-    <div class="card">
-
-        <h1>
-            Pilihan anda sudah kami simpan,<br>
-            Terima kasih sudah berpartisipasi<br>
-            dalam voting ini.
-        </h1>
-
-        <button class="btn" onclick="window.location.href='index.php'">KEMBALI KE HALAMAN LOGIN</button>
-
-    </div>
-
-    
+     
     <img src="dua-orang.png" class="students">
-
-</div>
-
+      
 </body>
 </html>
