de875e61dd4f91164890a9c902f3bb8136e65724
corinaokta <corinaocktaviani248204@student.smkn1kandeman.sch.id>
2026-05-12 10:28:51 +0700

    update

--- config.php
+++ config.php
@@ -2,11 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-<<<<<<< HEAD
 $db   = "rpl_vote";
-=======
-$db   = "projek-votting";
->>>>>>> 97994c7ec3cb447996c0d188d73deef214f9554f
 
 $conn = new mysqli($host, $user, $pass, $db);
 
