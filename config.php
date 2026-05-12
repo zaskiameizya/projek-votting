272ef09b43245bcea93ef44bfcb0df9f8f9f6701
zaskiameizya <zaskiameisyaputri@gmail.com>
2026-05-12 12:47:56 +0700

    ubh

--- config.php
+++ config.php
@@ -2,11 +2,7 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-<<<<<<< HEAD
-$db   = "rpl_vote";
-=======
 $db   = "rpl-vote";
->>>>>>> 9c7b80441d03fc7f34692be9c3cec608a25f641c
 
 $conn = new mysqli($host, $user, $pass, $db);
 
